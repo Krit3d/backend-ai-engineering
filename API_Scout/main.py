218f3bdd21f68b2d5cfb1620e553de87a3ce71cf
Krit3d <kritsprog@gmail.com>
2026-03-05 20:44:16 +0500

    refactor: restructure API_Scout logic
    - Extracted price fetching to function get_actual_price
    
    - Added dynamic random selection for coins and currencies
    
    - Added delay between requests
    
    - Some minor fixes

--- API_Scout/main.py
+++ API_Scout/main.py
@@ -1,21 +1,42 @@
 import requests
+from random import choice
+from time import sleep
 
-url = "https://api.coingecko.com/api/v3/simple/price?ids=bitcoin&vs_currencies=usd"
 
-try:
-    response = requests.get(url)
-except requests.exceptions.RequestException as e:
-    # В случае исключения сообщаем об этом пользователю
-    print(f"Your request returned an exception: {type(e).__name__}.")
-    print("Please check your internet connection")
-else:
-    if response.status_code == 200:
-        # Превращаем JSON-ответ в словарь Python
-        data = response.json()
+def get_actual_price(coin=None, currency="usd"):
+    if coin is None:
+        coin = {"id": "bitcoin", "name": "Bitcoin"}
 
-        # Выводим по ключу
-        print(
-            f"The current value of 1 BTC($): {data.get("bitcoin", {}).get("usd", "No data")}"
-        )
+    url = f"https://api.coingecko.com/api/v3/simple/price?ids={coin['id']}&vs_currencies={currency}"
+
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        # В случае исключения сообщаем об этом пользователю
+        print(f"Your request returned an exception: {type(e).__name__}.")
+        print("Please check your internet connection")
     else:
-        print("Server Error. Please wait or check if your request is right")
+        if response.status_code == 200:
+            # Превращаем JSON-ответ в словарь Python
+            data = response.json()
+
+            # Возваращаем искомую цену
+            return f"The current value of 1 {coin['name']} is {data.get(coin['id'], {}).get(currency, 'No data')} {currency.upper()}."
+
+        return "Server Error. Please wait or check if your request is right"
+
+
+# Список всех монет
+COINS_LIST = requests.get("https://api.coingecko.com/api/v3/coins/list").json()
+# Делаем задержку между запросами(хотя она тут не особо поможет :))
+sleep(1)
+# Список котируемых валют
+COUNTER_CURRENCIES = requests.get(
+    "https://api.coingecko.com/api/v3/simple/supported_vs_currencies"
+).json()
+
+print(get_actual_price(choice(COINS_LIST), choice(COUNTER_CURRENCIES)))
+sleep(1)
+print(get_actual_price(choice(COINS_LIST), choice(COUNTER_CURRENCIES)))
+sleep(1)
+print(get_actual_price(choice(COINS_LIST), choice(COUNTER_CURRENCIES)))
