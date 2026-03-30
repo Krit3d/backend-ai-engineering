d29798873142511e1840bd1bfe4d3de07a41c48e
Krit3d <kritsprog@gmail.com>
2026-03-30 21:37:47 +0500

    feat: add POST endpoint with data validation

--- FastAPI_Start/main.py
+++ FastAPI_Start/main.py
@@ -1,9 +1,27 @@
 from fastapi import FastAPI
 from fastapi.responses import JSONResponse
+from pydantic import BaseModel, EmailStr
+
+
+class UserCreate(BaseModel):
+    username: str
+    age: int
+    email: EmailStr
+
 
 app = FastAPI()
 
 
 @app.get("/")
-async def root():
+async def root() -> JSONResponse:
     return JSONResponse(content={"status": "Ed is in prod"})
+
+
+@app.post("/users")
+async def create_user(user: UserCreate) -> JSONResponse:
+    return JSONResponse(
+        content={
+            "message": f"User {user.username} successfully created!",
+            "user": user.model_dump(),
+        }
+    )
